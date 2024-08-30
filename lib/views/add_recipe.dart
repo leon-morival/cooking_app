d1ca11bc942b82b2c54590b31a4331c95204ddc7
leoncci <leon.morival@gmail.com>
2024-08-30 20:56:14 +0200

    fetch recipes on home page

--- lib/views/add_recipe.dart
+++ lib/views/add_recipe.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cooking_app/models/recipe_model.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddRecipePage extends StatefulWidget {
   const AddRecipePage({super.key});
@@ -13,10 +16,59 @@ class _AddRecipePageState extends State<AddRecipePage> {
   final _formKey = GlobalKey<FormState>();
 
   final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _imageURLController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
   final TextEditingController _ingredientsController = TextEditingController();
   final TextEditingController _stepsController = TextEditingController();
+
+  File? _imageFile; // Variable to hold the selected image
+
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _pickImage(ImageSource source) async {
+    try {
+      final XFile? pickedFile = await _picker.pickImage(source: source);
+      if (pickedFile != null) {
+        setState(() {
+          _imageFile = File(pickedFile.path);
+        });
+      }
+    } catch (e) {
+      print("Failed to pick image: $e");
+    }
+  }
+
+  void _showImageSourceSelection() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Select Image Source'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                _pickImage(ImageSource.camera); // Take a photo
+              },
+              child: const Text('Take a Photo'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                _pickImage(ImageSource.gallery); // Pick from gallery
+              },
+              child: const Text('Choose from Gallery'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,22 +82,20 @@ class _AddRecipePageState extends State<AddRecipePage> {
           key: _formKey,
           child: ListView(
             children: [
+              // Image Picker
+              _imageFile == null
+                  ? const Text('No image selected.')
+                  : Image.file(_imageFile!),
+              ElevatedButton(
+                onPressed: _showImageSourceSelection,
+                child: const Text('Pick Image'),
+              ),
               TextFormField(
                 controller: _nameController,
                 decoration: const InputDecoration(labelText: 'Recipe Name'),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter the recipe name';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _imageURLController,
-                decoration: const InputDecoration(labelText: 'Image URL'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter the image URL';
                   }
                   return null;
                 },
@@ -99,10 +149,21 @@ class _AddRecipePageState extends State<AddRecipePage> {
   }
 
   void _addRecipe() async {
+    if (_imageFile == null) {
+      // Handle case where no image is selected
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Please select an image.'),
+        ),
+      );
+      return;
+    }
+
     final newRecipe = Recipe(
       id: '',
       name: _nameController.text,
-      imageURL: _imageURLController.text,
+      imageURL: _imageFile!
+          .path, // Save the file path or upload the file to Firebase Storage
       description: _descriptionController.text,
       ingredients: _ingredientsController.text.split(','),
       steps: _stepsController.text.split(','),
@@ -111,23 +172,34 @@ class _AddRecipePageState extends State<AddRecipePage> {
       authorId: 'exampleAuthorId', // Replace with actual author ID
     );
 
-    final docRef = await FirebaseFirestore.instance
-        .collection('recipes')
-        .add(newRecipe.toMap());
+    try {
+      final docRef = await FirebaseFirestore.instance
+          .collection('recipes')
+          .add(newRecipe.toMap());
 
-    // Optionally update the recipe with the generated ID
-    await FirebaseFirestore.instance
-        .collection('recipes')
-        .doc(docRef.id)
-        .update({'id': docRef.id});
+      // Optionally update the recipe with the generated ID
+      await FirebaseFirestore.instance
+          .collection('recipes')
+          .doc(docRef.id)
+          .update({'id': docRef.id});
 
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text("La recette à bien été ajoutée!"),
-        duration: Duration(seconds: 2),
-      ),
-    );
+      // Show success message
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Recipe added successfully!'),
+          duration: Duration(seconds: 2),
+        ),
+      );
 
-    Navigator.pop(context);
+      Navigator.pop(context);
+    } catch (e) {
+      // Handle any errors
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to add recipe: $e'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    }
   }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cooking_app/views/recipe_detail_page';
 import 'package:flutter/material.dart';
+import 'package:cooking_app/models/recipe_model.dart';
+import 'dart:io';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +14,85 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Les plus populaires',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('recipes').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text('No recipes found.'));
+          }
+
+          final recipes = snapshot.data!.docs.map((doc) {
+            final data = doc.data() as Map<String, dynamic>;
+            return Recipe.fromFirestore(data, doc.id);
+          }).toList();
+
+          return ListView.builder(
+            itemCount: recipes.length,
+            itemBuilder: (context, index) {
+              final recipe = recipes[index];
+              return Container(
+                margin:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200], // Light grey background color
+                  borderRadius: BorderRadius.circular(12.0), // Border radius
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 4,
+                      offset: const Offset(0, 2), // Shadow offset
+                    ),
+                  ],
+                ),
+                child: ListTile(
+                  contentPadding: const EdgeInsets.all(8.0),
+                  leading: recipe.imageURL.isNotEmpty
+                      ? Image.file(
+                          File(recipe.imageURL),
+                          width: 100,
+                          height: 50,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) {
+                            return const Icon(Icons.image,
+                                size: 50); // Fallback icon
+                          },
+                        )
+                      : const Icon(Icons.image, size: 50),
+                  title: Text(recipe.name),
+                  subtitle: Text(recipe.description),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RecipeDetailPage(recipe: recipe),
+                      ),
+                    );
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
